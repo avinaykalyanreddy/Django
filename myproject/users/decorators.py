8dfd2a0122d8c0e6ff03e28513c79249129ee450
A VINAY KALYAN REDDY <114601857+avinaykalyanreddy@users.noreply.github.com>
2025-08-29 19:41:40 +0100

    Add files via upload
    this folder contains custom user login and log out

---
+++ myproject/users/decorators.py
@@ -0,0 +1,16 @@
+
+from django.shortcuts import redirect
+
+def login_required_custom(view_func):
+
+
+
+    def wrapper(request,*args,**kwargs):
+
+        if not request.session.get("user_id"):
+
+            return redirect("users:login")
+
+        return view_func(request,*args,**kwargs)
+
+    return wrapper
